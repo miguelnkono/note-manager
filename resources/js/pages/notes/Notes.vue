3ed47441fb4999a8489345126e7c2ead6d9ad1d8
Nkono Ndeme Miguel <nkonondememiguel7@gmail.com>
2025-09-30 02:35:22 +0100

    added smal modification on the layout of the notes index page

--- resources/js/pages/notes/Notes.vue
+++ resources/js/pages/notes/Notes.vue
@@ -23,24 +23,26 @@ const breadcrumbs: BreadcrumbItem[] = [
             <!-- Hero Content -->
             <div class="relative z-10 flex h-full flex-col items-center justify-center px-4">
                 <div class="max-w-3xl text-center">
-                    <h1 class="mb-8 text-4xl font-bold tracking-tight sm:text-6xl lg:text-7xl text-slate-900">
+                    <h1 class="mb-8 text-4xl font-bold tracking-tight text-slate-900 sm:text-6xl lg:text-7xl">
                         Start managing your
                         <span class="text-sky-900">Notes</span>
                     </h1>
                     <p class="mx-auto mb-8 max-w-2xl text-lg text-slate-700">
                         Note taking has never been so easily token care of! From creating, editing, reviewing to deleting, SmartNote has your back.
                     </p>
-                    <div class="flex flex-wrap justify-center gap-4">
-                        <Link :href="route('notes')" class="rounded-lg px-6 py-3 font-medium bg-sky-900 text-white hover:bg-sky-800">
+                    <div class="flex flex-wrap justify-center  flex-col gap-y-4 sm:flex-row sm:gap-4">
+                        <Link :href="route('notes')" class="rounded-lg bg-sky-900 px-6 py-3 font-medium text-white hover:bg-sky-800">
                             Create
                         </Link>
-                        <Link :href="route('notes')" class="rounded-lg border px-6 py-3 font-medium border-slate-200 bg-white text-slate-900 hover:bg-slate-50">
+                        <Link
+                            :href="route('notes')"
+                            class="rounded-lg border border-slate-200 bg-white px-6 py-3 font-medium text-slate-900 hover:bg-slate-50"
+                        >
                             View
                         </Link>
                     </div>
                 </div>
             </div>
         </div>
-
     </AppLayout>
 </template>
